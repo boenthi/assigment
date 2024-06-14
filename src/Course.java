ad94891cde0a173b5900fd44ac722090120bccd9
boenthi <boenthi989@gmil.com>
2024-06-14 13:25:47 +0700

    first commit

---
+++ src/Course.java
@@ -0,0 +1,24 @@
+package Course;
+
+import java.util.Collection;
+import java.util.Date;
+
+
+public class Course{
+    private Integer id;
+    private String courseTitle;
+    private String [] instructorNames;
+    private String [] courseRequirements;
+    private Date startedDate;
+
+    public Course(int id, String title) {
+    }
+
+    public Collection<Object> getTitle() {
+        return null;
+    }
+
+    public int getId() {
+        return 0;
+    }
+}
